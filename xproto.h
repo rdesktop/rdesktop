539255f7fd71c2683274dd5100d8d457ec0dc766
Erik Forsberg <forsberg@cendio.com>
2003-06-06 10:43:40 +0000

    Comments about what file contains what functions.
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/trunk/rdesktop@406 423420c4-83ab-492f-b58f-81f9feb106b5

--- xproto.h
+++ xproto.h
@@ -1,5 +1,7 @@
+/* cliprdr.c */
 void cliprdr_handle_SelectionNotify(XSelectionEvent * event);
 void cliprdr_handle_SelectionRequest(XSelectionRequestEvent * xevent);
+/* xwin.c */
 void xwin_register_propertynotify(Window event_wnd, Atom atom,
 				  void (*propertycallback) (XPropertyEvent *));
 void xwin_deregister_propertynotify(Window event_wnd, Atom atom);
