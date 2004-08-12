c609793f7fed2a2124b55d349f50ec2fb817a855
Jay Sorg <j@american-data.com.x>
2004-08-12 02:36:29 +0000

    added qterdesktopbuild.sh
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/trunk/rdesktop@749 423420c4-83ab-492f-b58f-81f9feb106b5

---
+++ uiports/qterdesktopbuild.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# qterdesktop build script
+
+rm qterdesktop
+rm *.o
+rm moc_*
+
+cc="g++"
+cflags="-pipe -DQT_QWS_EBX -DQT_QWS_CUSTOM -DQWS -fno-exceptions -fno-rtti -Wall -Os -fno-default-inline -DNO_DEBUG -DWITH_OPENSSL"
+# uncomment the following line to turn on debug
+#cflags=$cflags+" -DWITH_DEBUG"
+lflags="-lcrypto -L/usr/local/qt/lib -lqte"
+
+# rdesktop core files
+$cc $cflags -c ../tcp.c -o tcp.o
+$cc $cflags -c ../iso.c -o iso.o
+$cc $cflags -c ../mcs.c -o mcs.o
+$cc $cflags -c ../secure.c -o secure.o
+$cc $cflags -c ../rdp.c -o rdp.o
+$cc $cflags -c ../rdp5.c -o rdp5.o
+$cc $cflags -c ../orders.c -o orders.o
+$cc $cflags -c ../cache.c -o cache.o
+$cc $cflags -c ../mppc.c -o mppc.o
+$cc $cflags -c ../licence.c -o licence.o
+$cc $cflags -c ../bitmap.c -o bitmap.o
+$cc $cflags -c ../channels.c -o channels.o
+$cc $cflags -c ../pstcache.c -o pstcache.o
+
+# qt files
+$cc $cflags -I/usr/local/qt/include -c qtewin.cpp -o qtewin.o
+
+# moc stuff
+/usr/local/qt/bin/moc qtewin.h > moc_qtewin.cpp
+
+$cc $cflags -I/usr/local/qt/include -c moc_qtewin.cpp -o moc_qtewin.o
+
+$cc $lflags -o qterdesktop moc_qtewin.o qtewin.o tcp.o iso.o mcs.o secure.o rdp.o rdp5.o orders.o cache.o mppc.o licence.o bitmap.o channels.o pstcache.o
