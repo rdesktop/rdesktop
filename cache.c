845b56bdc983893669ec695259f98206ffbfd32a
Jay Sorg <j@american-data.com.x>
2004-10-02 00:40:13 +0000

    don't remove lru bitmap unless its a rdp5 connection
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/trunk/rdesktop@773 423420c4-83ab-492f-b58f-81f9feb106b5

--- cache.c
+++ cache.c
@@ -2,17 +2,17 @@
    rdesktop: A Remote Desktop Protocol client.
    Cache routines
    Copyright (C) Matthew Chapman 1999-2002
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -25,6 +25,7 @@
 #define IS_PERSISTENT(id) (g_pstcache_fd[id] > 0)
 
 extern int g_pstcache_fd[];
+extern BOOL g_use_rdp5;
 
 uint32 g_stamp;
 int g_num_bitmaps_in_memory[3];
@@ -98,7 +99,7 @@ cache_put_bitmap(uint8 cache_id, uint16 cache_idx, HBITMAP bitmap, uint32 stamp)
 		{
 			ui_destroy_bitmap(old);
 		}
-		else
+		else if (g_use_rdp5)
 		{
 			if (++g_num_bitmaps_in_memory[cache_id] > BMPCACHE2_C2_CELLS)
 				cache_remove_lru_bitmap(cache_id);
