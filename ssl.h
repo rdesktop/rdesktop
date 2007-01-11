fd4166de6c664f61541b0861a0c67b6e86092b3c
Jay Sorg <j@american-data.com.x>
2007-01-11 03:34:03 +0000

    ran indent-all.sh
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/trunk/rdesktop@1377 423420c4-83ab-492f-b58f-81f9feb106b5

--- ssl.h
+++ ssl.h
@@ -48,20 +48,17 @@ void ssl_md5_update(SSL_MD5 * md5, uint8 * data, uint32 len);
 void ssl_md5_final(SSL_MD5 * md5, uint8 * out_data);
 void ssl_rc4_set_key(SSL_RC4 * rc4, uint8 * key, uint32 len);
 void ssl_rc4_crypt(SSL_RC4 * rc4, uint8 * in_data, uint8 * out_data, uint32 len);
-void
-ssl_rsa_encrypt(uint8 * out, uint8 * in, int len, uint32 modulus_size, uint8 * modulus,
-		uint8 * exponent);
+void ssl_rsa_encrypt(uint8 * out, uint8 * in, int len, uint32 modulus_size, uint8 * modulus,
+		     uint8 * exponent);
 SSL_CERT *ssl_cert_read(uint8 * data, uint32 len);
 void ssl_cert_free(SSL_CERT * cert);
 SSL_RKEY *ssl_cert_to_rkey(SSL_CERT * cert, uint32 * key_len);
 RD_BOOL ssl_certs_ok(SSL_CERT * server_cert, SSL_CERT * cacert);
 int ssl_cert_print_fp(FILE * fp, SSL_CERT * cert);
 void ssl_rkey_free(SSL_RKEY * rkey);
-int
-ssl_rkey_get_exp_mod(SSL_RKEY * rkey, uint8 * exponent, uint32 max_exp_len, uint8 * modulus,
-		     uint32 max_mod_len);
-RD_BOOL
-ssl_sig_ok(uint8 * exponent, uint32 exp_len, uint8 * modulus, uint32 mod_len,
-	   uint8 * signature, uint32 sig_len);
+int ssl_rkey_get_exp_mod(SSL_RKEY * rkey, uint8 * exponent, uint32 max_exp_len, uint8 * modulus,
+			 uint32 max_mod_len);
+RD_BOOL ssl_sig_ok(uint8 * exponent, uint32 exp_len, uint8 * modulus, uint32 mod_len,
+		   uint8 * signature, uint32 sig_len);
 
 #endif
