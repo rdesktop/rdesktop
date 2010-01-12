91f3b6151f8edf7ffa8f95f8cc03d4ea6c3ed38e
Peter Åstrand <astrand@cendio.com>
2010-01-12 08:06:15 +0000

    Changed style of copyright header to match the other files
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1533 423420c4-83ab-492f-b58f-81f9feb106b5

--- printercache.c
+++ printercache.c
@@ -1,23 +1,23 @@
 /* -*- c-basic-offset: 8 -*-
- * rdesktop: A Remote Desktop Protocol client.
- * Entrypoint and utility functions
- * Copyright (C) Matthew Chapman 1999-2008
- * Copyright (C) Jeroen Meijer 2003-2008
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
+   rdesktop: A Remote Desktop Protocol client.
+   Entrypoint and utility functions
+   Copyright (C) Matthew Chapman 1999-2008
+   Copyright (C) Jeroen Meijer 2003-2008
+  
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+  
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+  
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 /* According to the W2K RDP Printer Redirection WhitePaper, a data
  * blob is sent to the client after the configuration of the printer
