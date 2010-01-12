5dbe5f0a052a72014e637b388e7111a5e8dc251c
Peter Åstrand <astrand@cendio.com>
2010-01-12 08:03:31 +0000

    Corrected license header
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1532 423420c4-83ab-492f-b58f-81f9feb106b5

--- lspci.c
+++ lspci.c
@@ -1,22 +1,22 @@
 /*  -*- c-basic-offset: 8 -*-
-+   rdesktop: A Remote Desktop Protocol client.
-+   Support for the Matrox "lspci" channel
-+   Copyright (C) 2005 Matrox Graphics Inc. 
-+
-+   This program is free software; you can redistribute it and/or modify
-+   it under the terms of the GNU General Public License as published by
-+   the Free Software Foundation; either version 2 of the License, or
-+   (at your option) any later version.
-+
-+   This program is distributed in the hope that it will be useful,
-+   but WITHOUT ANY WARRANTY; without even the implied warranty of
-+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-+   GNU General Public License for more details.
-+
-+   You should have received a copy of the GNU General Public License
-+   along with this program; if not, write to the Free Software
-+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-+*/
+   rdesktop: A Remote Desktop Protocol client.
+   Support for the Matrox "lspci" channel
+   Copyright (C) 2005 Matrox Graphics Inc. 
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #include "rdesktop.h"
 #include <sys/types.h>
