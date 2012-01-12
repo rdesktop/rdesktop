rdesktop/rdesktop

commit 1be5dbdf81ae7a224ad055a517f62a26e1c8fa88

Applied patch from tracker patched ID: 2812158 which implements the FileFsFullSizeInformation which solves the read-only issues with disk redirections on Windows 7 and Windows 2008R2.
git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1605 423420c4-83ab-492f-b58f-81f9feb106b5

disk.c
@@ -1086,6 +1086,18 @@ disk_query_volume_information(RD_NTHANDLE handle, uint32 info_class, STREAM out)
 			out_uint32_le(out, 0x200);	/* Bytes per sector */
 			break;
 
+		case FileFsFullSizeInformation:
+
+			out_uint32_le(out, stat_fs.f_blocks);   /* Total allocation units low */
+			out_uint32_le(out, 0);  /* Total allocation units high */
+			out_uint32_le(out, stat_fs.f_blocks);   /* Caller allocation units low */
+			out_uint32_le(out, 0);  /* Caller allocation units high */
+			out_uint32_le(out, stat_fs.f_bfree);    /* Available allocation units */
+			out_uint32_le(out, 0);  /* Available allowcation units */
+			out_uint32_le(out, stat_fs.f_bsize / 0x200);    /* Sectors per allocation unit */
+			out_uint32_le(out, 0x200);      /* Bytes per sector */
+			break;
+
 		case FileFsAttributeInformation:
 
 			out_uint32_le(out, FS_CASE_SENSITIVE | FS_CASE_IS_PRESERVED);	/* fs attributes */
@@ -1098,7 +1110,6 @@ disk_query_volume_information(RD_NTHANDLE handle, uint32 info_class, STREAM out)
 		case FileFsLabelInformation:
 		case FileFsDeviceInformation:
 		case FileFsControlInformation:
-		case FileFsFullSizeInformation:
 		case FileFsObjectIdInformation:
 		case FileFsMaximumInformation:
 

commit 4dc2ce1a6a2afabac618895222ded1d78cb861f5

Fixes a typo introduced in r1605.
git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1657 423420c4-83ab-492f-b58f-81f9feb106b5

disk.c
@@ -1137,7 +1137,7 @@ disk_query_volume_information(RD_NTHANDLE handle, uint32 info_class, STREAM out)
 
 			out_uint32_le(out, stat_fs.f_blocks);	/* Total allocation units low */
 			out_uint32_le(out, 0);	/* Total allocation units high */
-			out_uint32_le(out, stat_fs.f_blocks);	/* Caller allocation units low */
+			out_uint32_le(out, stat_fs.f_bavail);	/* Caller allocation units low */
 			out_uint32_le(out, 0);	/* Caller allocation units high */
 			out_uint32_le(out, stat_fs.f_bfree);	/* Available allocation units */
 			out_uint32_le(out, 0);	/* Available allowcation units */