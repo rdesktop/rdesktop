83c4ea4c166b9c802990d3e5b21601b7ec3fbfea
Henrik Andersson <hean01@cendio.com>
2012-11-16 22:17:36 +0000

    Fixes the copyright of new code.
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1678 423420c4-83ab-492f-b58f-81f9feb106b5

--- asn.c
+++ asn.c
@@ -1,7 +1,7 @@
 /* -*- c-basic-offset: 8 -*-
    rdesktop: A Remote Desktop Protocol client.
    ASN.1 utility functions
-   Copyright 2010-2011 Henrik Andersson <hean01@cendio.se> for Cendio AB
+   Copyright 2012 Henrik Andersson <hean01@cendio.se> for Cendio AB
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- cssp.c
+++ cssp.c
@@ -1,3 +1,22 @@
+/* -*- c-basic-offset: 8 -*-
+   rdesktop: A Remote Desktop Protocol client.
+   CredSSP layer and kerberos support.
+   Copyright 2012 Henrik Andersson <hean01@cendio.se> for Cendio AB
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <gssapi.h>
 #include <gssapi/gssapi_krb5.h>
 #include "rdesktop.h"
