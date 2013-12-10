c618390bbe528dfcd7d2798dcfc7d5b7f614292f
Henrik Andersson <hean01@cendio.com>
2013-12-10 11:16:56 +0000

    Break out of rdp main loop if server sent a redirect PDU. This is needed on 2012r2 were server will not disconnect a rdp connection after a redirect packet.
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1777 423420c4-83ab-492f-b58f-81f9feb106b5

--- rdp.c
+++ rdp.c
@@ -1670,7 +1670,7 @@ rdp_main_loop(RD_BOOL * deactivated, uint32 * ext_disc_reason)
 {
 	while (rdp_loop(deactivated, ext_disc_reason))
 	{
-		if (g_pending_resize)
+		if (g_pending_resize || g_redirect)
 		{
 			return;
 		}
