dbb87244dd1cf6951b8f8abb4b9ebdc5290e8a43
Henrik Andersson <hean01@cendio.com>
2014-02-07 08:44:55 +0000

    Reset license issued state to make redirections work with issued device licenses.
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1785 423420c4-83ab-492f-b58f-81f9feb106b5

--- secure.c
+++ secure.c
@@ -930,6 +930,7 @@ sec_reset_state(void)
 	g_server_rdp_version = 0;
 	g_sec_encrypt_use_count = 0;
 	g_sec_decrypt_use_count = 0;
+	g_licence_issued = 0;
 	g_licence_error_result = 0;
 	mcs_reset_state();
 }
