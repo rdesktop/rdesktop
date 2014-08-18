d70b65edc78c79f0ebea2c81d4aaacf1f80301af
Henrik Andersson <hean01@cendio.com>
2014-08-18 07:47:07 +0000

    Revert of commit 1794. Fixes regression with "disconnect: Invalid licensing message."
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1823 423420c4-83ab-492f-b58f-81f9feb106b5

--- rdp.c
+++ rdp.c
@@ -1733,14 +1733,23 @@ RD_BOOL
 rdp_connect(char *server, uint32 flags, char *domain, char *password,
 	    char *command, char *directory, RD_BOOL reconnect)
 {
+	RD_BOOL deactivated = False;
+	uint32 ext_disc_reason = 0;
+
 	if (!sec_connect(server, g_username, domain, password, reconnect))
 		return False;
 
 	rdp_send_logon_info(flags, domain, g_username, password, command, directory);
 
-	if (g_network_error)
-		return False;
+	/* run RDP loop until first licence demand active PDU */
+	while (!g_rdp_shareid)
+	{
+		if (g_network_error)
+			return False;
 
+		if (!rdp_loop(&deactivated, &ext_disc_reason))
+			return False;
+	}
 	return True;
 }
 
