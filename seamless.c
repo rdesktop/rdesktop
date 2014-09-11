1b50eb1e42a792a19cf42110ea821ae66bda5d16
Peter Åstrand <astrand@cendio.com>
2014-09-11 06:36:25 +0000

    diag message spelling fix: persitent > persistent
    git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1829 423420c4-83ab-492f-b58f-81f9feb106b5

--- seamless.c
+++ seamless.c
@@ -549,7 +549,7 @@ seamless_send_persistent(RD_BOOL enable)
 	unsigned int res;
 	if (!g_seamless_rdp)
 		return (unsigned int) -1;
-	printf("%s persitent seamless mode.\n", enable?"Enable":"Disable");
+	printf("%s persistent seamless mode.\n", enable?"Enable":"Disable");
 	res = seamless_send("PERSISTENT", "%d", enable);
 	
 	return res;
