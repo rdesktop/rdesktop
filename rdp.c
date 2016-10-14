rdesktop/rdesktop

commit 78a4f41f48d91a3938291cafa5c90e3bd78d1aa6

Fix sigsegv while using credssp and kerberos without specifying domainname as argument.
git-svn-id: svn://svn.code.sf.net/p/rdesktop/code/rdesktop/trunk@1824 423420c4-83ab-492f-b58f-81f9feb106b5

rdp.c
@@ -189,6 +189,9 @@ rdp_send_data(STREAM s, uint8 data_pdu_type)
 void
 rdp_out_unistr(STREAM s, char *string, int len)
 {
+	if (string == NULL || len == 0)
+		return;
+
 #ifdef HAVE_ICONV
 	size_t ibl = strlen(string), obl = len + 2;
 	static iconv_t iconv_h = (iconv_t) - 1;

commit c7369052cc7951958c0d9d3030136e97b88bac9e

Regression in RDP_V4 logon packet introduced by commit 78a4f41.
Commit 78a4f41, forces the caller to handle the write of empty string byte
where protocol needs it. This commit fixes the RDP V4 logon packet which needs
a mandatory 2 bytes null terminator to be written if strings are empty.

Fixes issue #67

rdp.c
@@ -379,11 +379,31 @@ rdp_send_logon_info(uint32 flags, char *domain, char *user,
 		out_uint16_le(s, len_password);
 		out_uint16_le(s, len_program);
 		out_uint16_le(s, len_directory);
-		rdp_out_unistr(s, domain, len_domain);
-		rdp_out_unistr(s, user, len_user);
-		rdp_out_unistr(s, password, len_password);
-		rdp_out_unistr(s, program, len_program);
-		rdp_out_unistr(s, directory, len_directory);
+
+		if (0 < len_domain)
+			rdp_out_unistr(s, domain, len_domain);
+		else
+			out_uint16_le(s, 0);
+
+		if (0 < len_user)
+			rdp_out_unistr(s, user, len_user);
+		else
+			out_uint16_le(s, 0);
+
+		if (0 < len_password)
+			rdp_out_unistr(s, password, len_password);
+		else
+			out_uint16_le(s, 0);
+
+		if (0 < len_program)
+			rdp_out_unistr(s, program, len_program);
+		else
+			out_uint16_le(s, 0);
+
+		if (0 < len_directory)
+			rdp_out_unistr(s, directory, len_directory);
+		else
+			out_uint16_le(s, 0);
 	}
 	else
 	{