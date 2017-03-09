rdesktop/rdesktop

commit 3140824be37ee4f02adbb235209015ea796233f0

-g 70%x90%

xwin.c
@@ -1953,8 +1953,8 @@ ui_init_connection(void)
 		/* Percent of screen */
 		if (-g_sizeopt >= 100)
 			g_using_full_workarea = True;
-		g_height = HeightOfScreen(g_screen) * (-g_sizeopt) / 100;
-		g_width = WidthOfScreen(g_screen) * (-g_sizeopt) / 100;
+		g_height = HeightOfScreen(g_screen) * (-g_height) / 100;
+		g_width = WidthOfScreen(g_screen) * (-g_width) / 100;
 	}
 	else if (g_sizeopt == 1)
 	{

doc/rdesktop.1
@@ -66,7 +66,7 @@ so its use is discouraged.
 Desktop geometry (WxH). If geometry is the special word "workarea", the geometry
 will be fetched from the extended window manager hints property _NET_WORKAREA, from
 the root window. The geometry can also be specified as a percentage of the whole
-screen, e.g. "-g 80%". 
+screen, e.g. "-g 80%", "-g 80%x70%". 
 
 If the specified geometry depends on the screen size, and the screen
 size is changed, rdesktop will automatically reconnect using the new

rdesktop.c
@@ -682,7 +682,14 @@ main(int argc, char *argv[])
 				if (*p == '%')
 				{
 					g_sizeopt = -g_width;
-					g_width = 800;
+					g_width = g_sizeopt;
+
+				    if (*(p + 1) == 'x'){
+                        g_height = -strtol(p + 2, &p, 10);
+                    }else{
+                        g_height = g_sizeopt;
+                    }
+
 					p++;
 				}
 

commit 2c9a706c23b13180bd2c6bed373fe2152c79a950

Fix X error BadAlloc when using seamlessrdp
The introduction of setting session size using percentages of both
with and height of screen like -g 50%x100%, commit 3140824be
introduced a bug when using seamless rdp. This commit fixes this bug.

xwin.c
@@ -1961,6 +1961,13 @@ ui_init_connection(void)
 		/* Percent of screen */
 		if (-g_sizeopt >= 100)
 			g_using_full_workarea = True;
+
+		if (g_width > 0)
+			g_width = g_sizeopt;
+
+		if (g_height > 0)
+			g_height = g_sizeopt;
+
 		g_height = HeightOfScreen(g_screen) * (-g_height) / 100;
 		g_width = WidthOfScreen(g_screen) * (-g_width) / 100;
 	}