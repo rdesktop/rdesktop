e1537061bf26ecfaee221f52c69db49153650d42
Pierre Ossman <ossman@cendio.se>
2019-04-09 14:03:16 +0200

    Fix packet debug output in rdp_protocol_error()
    We're trying to print the entire packet, not just what's left.

--- rdp.c
+++ rdp.c
@@ -2117,6 +2117,6 @@ _rdp_protocol_error(const char *file, int line, const char *func,
 {
 	logger(Protocol, Error, "%s:%d: %s(), %s", file, line, func, message);
 	if (s)
-		hexdump(s->p, s_length(s));
+		hexdump(s->data, s_length(s));
 	exit(0);
 }
